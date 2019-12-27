caf430dcb8cc60f2d6ba56e5aa8ef9c297d29b9b
MCozhusheck <mackozuszek@gmail.com>
2019-12-27 16:14:12 +0100

    Simple DB and auth

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require("express");
-const app = express();
-const port = 3001;
+const userRouter = require("./routes/user");
+const port = process.env.PORT;
+require("./db/db");
 const nms = require("./ServiceLiveStream");
+
+const app = express();
+app.use(express.json());
+app.use(userRouter);
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
-  nms.run();
+  //nms.run();
 });
